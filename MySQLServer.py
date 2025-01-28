aa14026d225618e038340044cb853b8811a54c0f
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-01-28 21:48:34 +0200

    1. Let's Build Your Database: Your Gateway to Data Adventure! ############################## Write a simple python script that creates the database alx_book_store in your MySQL server. ##############################

--- MySQLServer.py
+++ MySQLServer.py
@@ -12,5 +12,5 @@ if mydb.is_connected():
 
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
-    except Error as e:
-        print(f"Error: {e}")
+    except mysql.connector.Error:
+        print(f"Error: {Error}")
