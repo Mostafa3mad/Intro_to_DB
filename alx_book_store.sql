43e454bca3aec08efd097131529a40f78748d970
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-01-28 23:29:23 +0200

    0. A Magical Database for Your Dream Online Bookstore Adventure!
    ##############################
    Imagine you’re tasked with designing a MySQL database for an online bookstore. The database should store information about books, authors, customers, orders, and order details. Here’s an overview of the database schema:
    
    ##############################

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -15,7 +15,7 @@ CREATE TABLE Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE Customers (
+CREATE TABLE customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL UNIQUE,
